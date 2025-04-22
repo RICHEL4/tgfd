7a7cd71a7e95bb91fead4ae5eb1439d6d62fa30f
RICHEL4 <imbinintsoanah@gmail.com>
2025-04-22 19:45:52 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ_bmwbmw",
+  apiKey: "AIzaSyBZ_ffT5PXmyVNG8rWEstKy64oY2YG1_ec",
   authDomain: "tfgs-60078.firebaseapp.com",
   projectId: "tfgs-60078",
   storageBucket: "tfgs-60078.firebasestorage.app",
   messagingSenderId: "807659600665",
-  appId: "12345678910",
+  appId: "1:807659600665:web:a01f452b60e411fa0aa388",
   measurementId: "G-JE2TRHCE19"
 };
 
